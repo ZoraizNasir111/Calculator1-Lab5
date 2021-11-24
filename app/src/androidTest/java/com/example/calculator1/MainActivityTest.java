20798b0d46186255bdadbf81c227406832e03021
zoraiz <zoraiznasir11@hotmail.com>
2021-11-24 18:05:19 -0500

    Added three Instrument Tests - testing the UI of the app

---
+++ app/src/androidTest/java/com/example/calculator1/MainActivityTest.java
@@ -0,0 +1,275 @@
+package com.example.calculator1;
+
+
+import androidx.test.espresso.DataInteraction;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import static androidx.test.InstrumentationRegistry.getInstrumentation;
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
+import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static androidx.test.espresso.action.ViewActions.*;
+import static androidx.test.espresso.assertion.ViewAssertions.*;
+import static androidx.test.espresso.matcher.ViewMatchers.*;
+
+import com.example.calculator1.R;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void testUiComponents() {
+        ViewInteraction editText = onView(
+                allOf(withId(R.id.editText),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.view.ViewGroup.class))),
+                        isDisplayed()));
+        editText.check(matches(isDisplayed()));
+
+        ViewInteraction button = onView(
+                allOf(withId(R.id.buequal), withText("="),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class))),
+                        isDisplayed()));
+        button.check(matches(isDisplayed()));
+
+        ViewInteraction button2 = onView(
+                allOf(withId(R.id.buclear), withText("C"),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.TableLayout.class))),
+                        isDisplayed()));
+        button2.check(matches(isDisplayed()));
+
+        ViewInteraction button3 = onView(
+                allOf(withId(R.id.budot), withText("."),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.TableLayout.class))),
+                        isDisplayed()));
+        button3.check(matches(isDisplayed()));
+
+        ViewInteraction button4 = onView(
+                allOf(withId(R.id.divideBTN), withText("/"),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.TableLayout.class))),
+                        isDisplayed()));
+        button4.check(matches(isDisplayed()));
+
+        ViewInteraction button5 = onView(
+                allOf(withId(R.id.bu7), withText("7"),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.TableLayout.class))),
+                        isDisplayed()));
+        button5.check(matches(isDisplayed()));
+
+        ViewInteraction button6 = onView(
+                allOf(withId(R.id.bu0), withText("0"),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class))),
+                        isDisplayed()));
+        button6.check(matches(isDisplayed()));
+
+        ViewInteraction button7 = onView(
+                allOf(withId(R.id.bu2), withText("2"),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.TableLayout.class))),
+                        isDisplayed()));
+        button7.check(matches(isDisplayed()));
+
+        ViewInteraction button8 = onView(
+                allOf(withId(R.id.bu3), withText("3"),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.TableLayout.class))),
+                        isDisplayed()));
+        button8.check(matches(isDisplayed()));
+
+        ViewInteraction button9 = onView(
+                allOf(withId(R.id.plusBTN), withText("+"),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.TableLayout.class))),
+                        isDisplayed()));
+        button9.check(matches(isDisplayed()));
+
+        ViewInteraction button10 = onView(
+                allOf(withId(R.id.minusBTN), withText("-"),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.TableLayout.class))),
+                        isDisplayed()));
+        button10.check(matches(isDisplayed()));
+
+        ViewInteraction button11 = onView(
+                allOf(withId(R.id.multiplyBTN), withText("*"),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.TableLayout.class))),
+                        isDisplayed()));
+        button11.check(matches(isDisplayed()));
+
+        ViewInteraction button12 = onView(
+                allOf(withId(R.id.bu9), withText("9"),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.TableLayout.class))),
+                        isDisplayed()));
+        button12.check(matches(isDisplayed()));
+
+        ViewInteraction button13 = onView(
+                allOf(withId(R.id.bu6), withText("6"),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.TableLayout.class))),
+                        isDisplayed()));
+        button13.check(matches(isDisplayed()));
+
+        ViewInteraction button14 = onView(
+                allOf(withId(R.id.bu5), withText("5"),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.TableLayout.class))),
+                        isDisplayed()));
+        button14.check(matches(isDisplayed()));
+
+        ViewInteraction button15 = onView(
+                allOf(withId(R.id.bu8), withText("8"),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.TableLayout.class))),
+                        isDisplayed()));
+        button15.check(matches(isDisplayed()));
+
+        ViewInteraction materialButton = onView(
+                allOf(withId(R.id.bu7), withText("7"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.TableLayout")),
+                                        1),
+                                0),
+                        isDisplayed()));
+        materialButton.perform(click());
+
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.editText), withText("7"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
+                                        0),
+                                0),
+                        isDisplayed()));
+        appCompatEditText.perform(click());
+
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.editText), withText("7"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
+                                        0),
+                                0),
+                        isDisplayed()));
+        appCompatEditText2.perform(click());
+
+
+    }
+    @Test
+    public void testAdditionOf1and1() {
+        ViewInteraction materialButton = onView(
+                allOf(withId(R.id.bu1), withText("1"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.TableLayout")),
+                                        3),
+                                0),
+                        isDisplayed()));
+        materialButton.perform(click());
+
+        ViewInteraction materialButton2 = onView(
+                allOf(withId(R.id.plusBTN), withText("+"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.TableLayout")),
+                                        3),
+                                3),
+                        isDisplayed()));
+        materialButton2.perform(click());
+
+        ViewInteraction materialButton3 = onView(
+                allOf(withId(R.id.bu1), withText("1"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.TableLayout")),
+                                        3),
+                                0),
+                        isDisplayed()));
+        materialButton3.perform(click());
+
+        ViewInteraction materialButton4 = onView(
+                allOf(withId(R.id.buequal), withText("="),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        2),
+                                1),
+                        isDisplayed()));
+        materialButton4.perform(click());
+
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.editText), withText("2"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
+                                        0),
+                                0),
+                        isDisplayed()));
+        appCompatEditText.perform(click());
+
+        ViewInteraction editText = onView(
+                allOf(withId(R.id.editText), withText("2"),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.view.ViewGroup.class))),
+                        isDisplayed()));
+        editText.check(matches(withText("2")));
+    }
+    @Test
+    public void testEqualsDoesNotCrashApp() {
+        ViewInteraction materialButton = onView(
+                allOf(withId(R.id.buequal), withText("="),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        2),
+                                1),
+                        isDisplayed()));
+        materialButton.perform(click());
+
+        ViewInteraction editText = onView(
+                allOf(withId(R.id.editText),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.view.ViewGroup.class))),
+                        isDisplayed()));
+        editText.check(matches(isDisplayed()));
+
+        ViewInteraction editText2 = onView(
+                allOf(withId(R.id.editText),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.view.ViewGroup.class))),
+                        isDisplayed()));
+        editText2.check(matches(isDisplayed()));
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
